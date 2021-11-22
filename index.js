d1369b08857136a734951e7909da7750ead5c7b4
Chris Langenberg <speedmaze@gmail.com>
2021-11-22 18:18:22 +0100

    Refactored the Code
    * moved everything to a seperate file
    * add more layers to the NN
    * removed unused canvas package
    * save and load model from disk
    * added some data sources

--- index.js
+++ index.js
@@ -1,119 +1,37 @@
-console.log("Hello Autoencoder 🚂");
+import { Model } from './model.js'
+import { MnistDataSource } from './mnistDataSource.js'
+import { ImageTransformer } from './ImageTransformer.js'
+import { RandomDataSource } from './randomDataSource.js'
+import { ArbitraryImageDataSource } from './arbitraryImageDataSource.js'
 
-import * as tf from "@tensorflow/tfjs-node";
-// import canvas from "canvas";
-// const { loadImage } = canvas;
-import Jimp from "jimp";
-import numeral from "numeral";
-
-main();
+main()
 
 async function main() {
-  // Build the model
-  const autoencoder = buildModel();
-  // load all image data
-  const images = await loadImages(550);
+    // Instantiate the model
+    const model = new Model()
 
-  // train the model
-  const x_train = tf.tensor2d(images.slice(0, 500));
-  await trainModel(autoencoder, x_train, 250);
+    // Instantiate a data source
+    const dataSource = new MnistDataSource()
+    // const dataSource = new RandomDataSource()
+    // const dataSource = new ArbitraryImageDataSource()
 
-  // test the model
-  const x_test = tf.tensor2d(images.slice(500));
-  await generateTests(autoencoder, x_test);
-}
+    // Instatiate the Image transformer
+    const transformer = new ImageTransformer()
 
-async function generateTests(autoencoder, x_test) {
-  const output = autoencoder.predict(x_test);
-  // output.print();
-
-  const newImages = await output.array();
-  for (let i = 0; i < newImages.length; i++) {
-    const img = newImages[i];
-    const buffer = [];
-    for (let n = 0; n < img.length; n++) {
-      const val = Math.floor(img[n] * 255);
-      buffer[n * 4 + 0] = val;
-      buffer[n * 4 + 1] = val;
-      buffer[n * 4 + 2] = val;
-      buffer[n * 4 + 3] = 255;
+    // Check if there is a pretrained model. If it exists load it, or train the model
+    if (model.pretrainedModelExists()) {
+        await model.load()
+    } else {
+        // Create the layers
+        model.configure()
+        // and train
+        await model.train(await dataSource.getTrainingData(), 200)
     }
-    const image = new Jimp(
-      {
-        data: Buffer.from(buffer),
-        width: 28,
-        height: 28,
-      },
-      (err, image) => {
-        const num = numeral(i).format("000");
-        image.write(`output/square${num}.png`);
-      }
-    );
-  }
-}
+    // Test the model with testing data from the data source
+    const testData = await dataSource.getTestData()
+    const autoEncodedImages = model.autoencode(testData)
 
-function buildModel() {
-  const autoencoder = tf.sequential();
-  // Build the model
-  autoencoder.add(
-    tf.layers.dense({
-      units: 256,
-      inputShape: [784],
-      activation: "relu",
-    })
-  );
-  autoencoder.add(
-    tf.layers.dense({
-      units: 128,
-      activation: "relu",
-    })
-  );
-
-  autoencoder.add(
-    tf.layers.dense({
-      units: 256,
-      activation: "sigmoid",
-    })
-  );
-
-  autoencoder.add(
-    tf.layers.dense({
-      units: 784,
-      activation: "sigmoid",
-    })
-  );
-  autoencoder.compile({
-    optimizer: "adam",
-    loss: "binaryCrossentropy",
-    metrics: ["accuracy"],
-  });
-  return autoencoder;
-}
-
-async function trainModel(autoencoder, x_train, epochs) {
-  await autoencoder.fit(x_train, x_train, {
-    epochs: epochs,
-    batch_size: 32,
-    shuffle: true,
-    verbose: true,
-  });
-}
-
-async function loadImages(total) {
-  const allImages = [];
-  for (let i = 0; i < total; i++) {
-    const num = numeral(i).format("000");
-    const img = await Jimp.read(`data/square${num}.png`);
-
-    let rawData = [];
-    for (let n = 0; n < 28 * 28; n++) {
-      let index = n * 4;
-      let r = img.bitmap.data[index + 0];
-      // let g = img.bitmap.data[n + 1];
-      // let b = img.bitmap.data[n + 2];
-      rawData[n] = r / 255.0;
-    }
-    allImages[i] = rawData;
-  }
-  return allImages;
+    // save the images to disk
+    transformer.toImages(testData.arraySync(), 'org')
+    transformer.toImages(autoEncodedImages.arraySync())
 }
