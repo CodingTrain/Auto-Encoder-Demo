c09ccc7b868658841e46ec72fceb54ae337573aa
kfahn22 <kfm722@gmail.com>
2022-01-05 13:51:43 -0500

    code runs with c=1, but fails to write if c=3

--- README.md
+++ README.md
@@ -12,3 +12,48 @@ Exploring autoencoders with p5 and tensorflow js
 ## Related topics
 * [Coding train tensorflow.js playlist](https://www.youtube.com/playlist?list=PLRqwX-V7Uu6YIeVA3dNxbR9PYj4wV31oQ)
 * [Coding train neural network playlist](https://www.youtube.com/playlist?list=PLRqwX-V7Uu6aCibgK1PTWWu9by6XFdCfh)
+
+
+## Introduction
+
+This definition of machine learning, adapted from "Deep Learning with Javascript:  Neural Networks in Tensorflow.js", encapsulates the purpose of the autoencoder nicely:  "finding an appropriate transformation that turns the old representation of the input data into a new one".  In this case, the data is the pixel values in a (28*28 or 56*56) image of randomly sized circles or aquares generated in Processing.  The role of the autoencoder is to develop a model which will learn how to produce new images based on a new input (a new shape, random noise, etc.).
+
+ The job of the autoencoder is to build a model 
+ 
+ the weights that will be applied to each neuron (pixel) in each layer.  Starting at the final layer, the weights are determined by calculating the difference between the predicted pixel array and the actual pixel array (error), for each image in a random batch of 32 images in the training data. Epochs specified how many times this process is repeated. This calculation is repeated in backwards fashion for each layer of the autoencoder. 
+
+ Once the weights are calculated, they are stored in a json file in the model folder.  Given the model weights, it is possible to create new images by imputing test data.  The test images could be images of circles or squares (with or without noise) or pure random noise. 
+
+Key concepts:
+
+1.  Neural Network:  composed of an input layer, one or more hidden layers, and an output layer Our input layer is the array containing the normalized r(g,b) values for each pixel
+
+2.  Gradient Descent:  A really good analogy is descending a steep mountain trail in the dark, when you only have the narrow beam of a flashlight to guide you. You will make small steps trying to get to the bottom of the mountain, taken from "Make Your Own Neural Network".  Each small step is analogous to the learning rate.  If there is only one mountain peak, given enough time you will eventually reach the bottom.  However, if there are many peaks with valleys, it is possible you will not reach the local value but not reach the base of the mountain.  For this reason, it is a best practice to train on small groups of samples (batches) and shuffle the samples.
+
+3. Drop out - random elements in the input tensor are dropped to add randomness tothe training and help prevent overfitting
+
+## Key Model Hyperparameters:
+
+1.  Activation Function The activation function is a nonlinear, differentiable function that is used to improve the efficiency of the model("relu", sigmoid) over plain linear regression.
+2.  Learning Rate:  the rate at which the weights are adjusted.
+3.  Optimizer:  adam
+4.  Input Shape:  number of nodes going into each layer, W*W
+5.  Units: number of nodes coming out of each layer.  The nodes 
+6.  Accuracy:  perceptange of correct predictions
+    Note:  can be misleading which is why loss is used to monitor how well the model is doing
+7.  Loss: "MeanSquaredError", in this case a measure of the difference between the actual image and predicted image.
+8.  Drop out rate - percentage of inputs dropped in a layer                                                                                                 
+
+
+## Training with color images
+
+Each image is a 3D tensor with (height-width-channel).  The batch of images is a 4D tensor, where the additional dimension is the number of images (NHWC)
+
+
+## Libraries 
+
+1.  Jimp:  used to read in pixel data from the images created in Processing/P5JS and write new images to a file.
+
+2.  Numeral: used to efficiently refer to each image during the read/write process
+
+  
--- index.js
+++ index.js
@@ -7,8 +7,12 @@ import Jimp from "jimp";
 import numeral from "numeral";
 
 
-const 
-W = 28;
+const W = 28;
+// variable for # of colors:  1 for greyscale and 3 for color
+const c = 1;
+
+//const images = await loadImages(1);
+
 
 main();
 
@@ -16,18 +20,22 @@ async function main() {
   // Build the model
   const { decoderLayers, autoencoder } = buildModel();
   // load all image data
-  const images = await loadImages(110);
+  const images = await loadImages(1100);
   
  // train the model
-  const x_train = tf.tensor2d(images.slice(0, 100));
-  await trainModel(autoencoder, x_train, 10);
+
+  const x_train = tf.tensor2d(images.slice(1000));//, [ 10, W, W, c ]);
+
+  //x_train = tf.util.flatten(x_train);
+  await trainModel(autoencoder, x_train, 20);
   const saveResults = await autoencoder.save("file://public/model/");
 
   console.log(autoencoder.summary());
 
   // const autoencoder = await tf.loadLayersModel("file://public/model/model.json");
   // test the model
-  const x_test = tf.tensor2d(images.slice(10));
+  //const x_test = tf.tensor2d(images.slice(100));
+  const x_test = tf.tensor2d(images.slice(100));//, [ 1, W , W, c ]);
   await generateTests(autoencoder, x_test);
 
   // Create a new model with just the decoder
@@ -37,8 +45,14 @@ async function main() {
 
 async function generateTests(autoencoder, x_test) {
   const output = autoencoder.predict(x_test);
-  output.print();
 
+  
+  //attempt at color: returning a tensor with rank 2 and shape [ 1, 2352]
+  //console.log(output.slice(0,1));
+  
+  //output.print();
+
+  //original code
   const newImages = await output.array();
   for (let i = 0; i < newImages.length; i++) {
     const img = newImages[i];
@@ -50,7 +64,29 @@ async function generateTests(autoencoder, x_test) {
       buffer[n * 4 + 2] = val;
       buffer[n * 4 + 3] = 255;
     }
-  
+
+  //new code getting r,g,b values
+  // const newImages = await output.array();
+  // for (let i = 0; i < newImages.length; i++) {
+  //   const img = newImages[i];
+  //   const buffer = [];
+  //   for (let n = 0; n < img.length; n++) {
+  //     //const val = Math.floor(img[n] * 255);
+  //     buffer[n * 4 + 0] = img[n * 4 + 0] * 255;
+  //     buffer[n * 4 + 1] = img[n * 4 + 1] * 255;
+  //     buffer[n * 4 + 2] = img[n * 4 + 2] * 255;
+  //     buffer[n * 4 + 3] = 255;
+      
+  //   }
+    
+    // let p = [];
+    // for (let n = 0; n < W*W; n++) {
+    // let idx = n * 4;
+    //   for (let cidx=0; cidx < c; cidx++) {
+    //     p.push(img.bitmap.data[idx+cidx]); 
+    //   rawData[n+cidx] = p[n+cidx] / 255.0;
+    //   }
+
     const image = new Jimp(
       {
         data: Buffer.from(buffer),
@@ -92,66 +128,66 @@ function buildModel() {
   // Encoder
   autoencoder.add(
     tf.layers.dense({
-      units: 1568,
-      inputShape: [W * W * 3],
+      units: 256*c,
+      inputShape: [ W*W*c ],
       activation: "relu",
     })
   );
   autoencoder.add(
     tf.layers.dense({
-      units: 784,
+      units: 128*c,
       activation: "relu",
     })
   );
   autoencoder.add(
     tf.layers.dense({
-      units: 392,
+      units: 64*c,
       activation: "relu",
     })
   );
   autoencoder.add(
     tf.layers.dense({
-      units: 196,
+      units: 16*c,
       activation: "relu",
     })
   );
   autoencoder.add(
     tf.layers.dense({
-      units: 4*4,
+      units: 4*c,
       activation: "sigmoid",
     })
   );
-  // How do I start from here?
-  // Decoder
+  // // How do I start from here?
+  // // Decoder
 
   let decoderLayers = [];
   decoderLayers.push(
     tf.layers.dense({
-      units: 16*4,
+      units: 16*c,
       activation: "relu",
     })
   );
   decoderLayers.push(
     tf.layers.dense({
-      units: 64*4,
+      units: 64*c,
       activation: "relu",
     })
   );
   decoderLayers.push(
     tf.layers.dense({
-      units: 128*4,
+      units: 128*c,
       activation: "relu",
     })
   );
   decoderLayers.push(
     tf.layers.dense({
-      units: 256*4,
+      units: 256*c,
       activation: "relu",
     })
   );
   decoderLayers.push(
     tf.layers.dense({
-      units: 2352,
+      units: W * W * c,
       activation: "sigmoid",
     })
   );
@@ -159,7 +195,6 @@ function buildModel() {
   for (let layer of decoderLayers) {
     autoencoder.add(layer);
   }
-
   // const learningRate = 0.001;
   // const optimizer = tf.train.adam(learningRate);
   autoencoder.compile({
@@ -180,46 +215,126 @@ async function trainModel(autoencoder, x_train, epochs) {
 
 async function loadImages(total) {
   const allImages = [];
+  let rawData = [];
   for (let i = 0; i < total; i++) {
     const num = numeral(i).format("0000");
     const img = await Jimp.read(
        `public/data/shape${num}.png`)
-      //  .then(img => {
-      //    return img 
-      //    .write('color.jpg');
-      //   })
-      //    .catch(err => {
-      //      console.error(err);
-       
-
-//original code pulling only r value 
-    // let rawData = [];
-    // for (let n = 0; n < W * W; n++) {
-    //   let index = n * 4;
-    //   let r = img.bitmap.data[index + 0];
-    //   let g = img.bitmap.data[index + 1];
-    //   let b = img.bitmap.data[index + 2];
-    //   rawData[r] = r / 255.0;
-    //   }
-    //   allImages[i] = rawData;
-    // }  
-    // return allImages;
-    // }
-    
-    const p = [];
-    const rawData = [];
-    img.scan(0,0, img.bitmap.width, img.bitmap.height, function(x,y, idx) {
-      p.push(this.bitmap.data[idx+0])
-      p.push(this.bitmap.data[idx+1])
-      p.push(this.bitmap.data[idx+2])
-    })
-    for (let n = 0; n < W*W*3; n++) {
-        rawData[n] = p[n] / 255.0;
-    };
-    allImages[i] = rawData;
-    //console.log(allImages[1]);
-  }
-  return allImages;
-}
+       .then(img => {
+         return img 
+          //.write('color.jpg');
+        })
+         .catch(err => {
+           console.error(err);
+         });
   
+    const p = [];
+    for (let n = 0; n < W*W; n++) {
+    let idx = n * 4;
+      for (let cidx=0; cidx < c; cidx++) {
+        p.push(img.bitmap.data[idx+cidx]);
+          rawData[n*c+cidx] = p[n*c+cidx] / 255.0;
+        }
+      }
+      //console.log(rawData) ; 
+      
+      allImages[i] = rawData;
+      //console.log(allImages[0]);
+    }
+    return allImages;
+    
+  }
+    
+//Maybe try in future
+//code adapted from https://github.com/tensorflow/tfjs-examples/blob/master/mnist/index.js
+  
+  // The first layer of the convolutional neural network plays a dual role:
+  // it is both the input layer of the neural network and a layer that performs
+  // the first convolution operation on the input. It receives the 28x28 pixels
+  // color images. This input layer uses 16 filters with a kernel size
+  // of 3 pixels each. It uses a simple RELU activation function which pretty
+  // much just looks like this: __/  Groups specifies the number of channels.
+  // autoencoder.add(tf.layers.conv2d({
+  //   inputShape: [W, W, c],
+  //   dataFormat: 'channelsLast',
+  //   kernelSize: 3,
+  //   filters: 16,
+  //   groups: 3,
+  //   activation: 'relu'
+  // }));
 
+  // // // After the first layer we include a MaxPooling layer. This acts as a sort of
+  // // // downsampling using max values in a region instead of averaging.
+  // // // https://www.quora.com/What-is-max-pooling-in-convolutional-neural-networks
+  // autoencoder.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+
+  // // // Our third layer is another convolution, this time with 32 filters.
+  // // autoencoder.add(tf.layers.conv4d({kernelSize: 3, filters: 32, activation: 'relu'}));
+
+  // // // Max pooling again.
+  // // autoencoder.add(tf.layers.maxPooling4d({poolSize: 2, strides: 2}));
+
+  // // // Add another conv2d layer.
+  // // autoencoder.add(tf.layers.conv4d({kernelSize: 3, filters: 32, activation: 'relu'}));
+
+  // // // Now we flatten the output from the 2D filters into a 1D vector to prepare
+  // // // it for input into our last layer. This is common practice when feeding
+  // // // higher dimensional data to a final classification output layer.
+  // autoencoder.add(tf.layers.flatten({}));
+
+  // // //add a dropout layer with a dropout rate of dr
+  // // autoencoder.add(tf.layers.dropout({ rate:  dr}));
+
+  // autoencoder.add(tf.layers.dense({units: 64, activation: 'relu'}));
+
+  // // // Our last layer is a dense layer which has 10 output units, one for each
+  // // // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9). Here the classes actually
+  // // // represent numbers, but it's the same idea if you had classes that
+  // // // represented other entities like dogs and cats (two output classes: 0, 1).
+  // // // We use the softmax function as the activation for the output layer as it
+  // // // creates a probability distribution over our 10 classes so their output
+  // // // values sum to 1.
+  // // autoencoder.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+  // // return model;
+
+  //   img.scan(0,0, img.bitmap.width, img.bitmap.height, function(x,y, idx) {
+  //     for (let cidx=0; cidx < c; cidx++) {
+  //       p.push()
+  //     //p.push(this.bitmap.data[idx+cidx])
+  //     }
+  //   })
+  //     for (let n = 0; n < W*W*c; n++) {
+  //     rawData[n]= p[n] / 255.0;
+  //     };
+  //     allImages[i] = rawData;
+  //     //console.log(allImages[0]);
+  //   } 
+  // return allImages;
+  // }
+
+
+//Failed Attemps to imput pixel array
+
+
+
+//   pixels = tf.node.decodePng(img);
+  //   rawData = pixels.div(255);
+  //   allImages[i] = rawData;
+  // }
+
+  // stackoverflow Jimp image to Tensor node.js
+//     const values = img.bitmap.data;
+//     const outShape = [1, img.bitmap.width, img.bitmap.height, 4];
+//     var input = tf.tensor(values, outShape);
+
+//     // // slice away alpha
+//     let rawInput = input.slice([0,0,0,0], [1, img.bitmap.width, img.bitmap.height, c]);
+//     let normInput = rawInput.div(255);
+//     console.log(normInput);
+//     //input = tf.util.flatten(input);
+//     allImages[i] = normInput;
+//     }
+//     console.log(allImages[0]);
+    
+//  return allImages;
+// }
